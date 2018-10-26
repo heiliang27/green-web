e6d5c15562b0e7e68a011281fcca665e6e3025fd
limingliang <limingliang@limingliang-PC>
2018-10-26 17:18:30 +0800

    使用spring-data-redis

--- src/main/java/com/green/common/config/RedisConfig.java
+++ src/main/java/com/green/common/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.green.common.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,27 +11,67 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.green.common.config.properties.RedisProperties;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class RedisConfig {
-	 @Bean
-	    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-	        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-	        redisTemplate.setConnectionFactory(redisConnectionFactory);
 
-	        // 使用Jackson2JsonRedisSerialize 替换默认序列化
-	        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+	@Autowired
+	private RedisProperties redisProperties;
 
-	        ObjectMapper objectMapper = new ObjectMapper();
-	        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-	        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+	/** JedisPoolConfig 连接池配置 */
+	@Bean
+	public JedisPoolConfig jedisPoolConfig() {
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		RedisProperties.Pool pool = this.redisProperties.getJedis().getPool();
+		if (pool != null) {
+			jedisPoolConfig = jedisPoolConfig(pool);
+		}
+		System.out.println("jedisPoolConfig--初始化----------------------------");
+		return jedisPoolConfig;
+	}
 
-	        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+	/** JedisPool连接池 */
+	@Bean
+	public JedisPool jedisPool(JedisPoolConfig jedisPoolConfig) {
+		JedisPool jedisPool = new JedisPool(jedisPoolConfig, redisProperties.getHost(), redisProperties.getPort(),
+				(int)redisProperties.getTimeout().toMillis());
+		System.out.println("jedisPool--初始化----------------------------");
+		return jedisPool;
+	}
 
-	        // 设置value的序列化规则和 key的序列化规则
-	        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-	        redisTemplate.setKeySerializer(new StringRedisSerializer());
-	        redisTemplate.afterPropertiesSet();
-	        return redisTemplate;
-	    }
+	@Bean
+	public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+		// 使用Jackson2JsonRedisSerialize 替换默认序列化
+		Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(
+				Object.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+		// 设置value的序列化规则和 key的序列化规则
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.afterPropertiesSet();
+		return redisTemplate;
+	}
+
+	private JedisPoolConfig jedisPoolConfig(RedisProperties.Pool pool) {
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxTotal(pool.getMaxActive());
+		config.setMaxIdle(pool.getMaxIdle());
+		config.setMinIdle(pool.getMinIdle());
+		if (pool.getMaxWait() != null) {
+			config.setMaxWaitMillis(pool.getMaxWait().toMillis());
+		}
+		return config;
+	}
+
 }
--- src/main/java/com/green/modular/controller/LoginController.java
+++ src/main/java/com/green/modular/controller/LoginController.java
@@ -5,15 +5,23 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
 @Controller
 public class LoginController {
 	
 	@Autowired
 	RedisTemplate<Object, Object> redisTemplate;
+	
+	@Autowired
+	JedisPool jedisPool;
 
 	@RequestMapping("/login")
-	public String login(){		
+	public String login(){	
+		Jedis jedis = jedisPool.getResource();
 		System.out.println(redisTemplate.opsForValue().get("lml"));
+		System.out.println("jedis="+jedis.get("lml"));
 		return "login.html";
 	}
 }
