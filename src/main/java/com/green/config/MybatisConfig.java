cfe2c897c11d1be4ae7bddcd55ab2b0b4508e3a2
limingliang <limingliang@licensing.ultraedit.com>
2018-12-11 19:46:26 +0800

    修改用户

--- src/main/java/com/green/config/MybatisConfig.java
+++ src/main/java/com/green/config/MybatisConfig.java
@@ -11,6 +11,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
--- src/main/java/com/green/modular/system/controller/UserController.java
+++ src/main/java/com/green/modular/system/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.green.modular.system.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.green.common.base.BaseController;
 import com.green.modular.system.entity.User;
 import com.green.modular.system.service.IUserService;
@@ -25,10 +27,15 @@ public class UserController extends BaseController {
 	IUserService userService;
 
 	/** 查询用户信息通过用户名称 */
-	@ResponseBody
 	@RequestMapping(value = "/account/{account}", method = RequestMethod.GET)
 	public User getUserByAccest(@PathVariable("account") String account) {
 		User user = userService.getUserByAccest(account);
 		return user;
 	}
+	
+	@RequestMapping("/list")
+	public List<User> list(){
+		return userService.list(null);
+	}
+	
 }
